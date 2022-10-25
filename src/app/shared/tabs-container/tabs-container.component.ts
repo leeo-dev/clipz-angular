326114d895c7561a7622ed84e80614e1bbf30e3c
Leonardo Albuquerque <leonardo@macrolabs.com.br>
2022-10-24 23:36:33 -0300

    feat: tabs funcionality

--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,11 +1,26 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, AfterContentInit, ContentChildren, QueryList } from '@angular/core'
+import { TabComponent } from '../tab/tab.component'
 
 @Component({
   selector: 'app-tabs-container',
   templateUrl: './tabs-container.component.html',
   styleUrls: ['./tabs-container.component.css']
 })
-export class TabsContainerComponent implements OnInit {
-  ngOnInit (): void {
+export class TabsContainerComponent implements AfterContentInit {
+  @ContentChildren(TabComponent) tabs?: QueryList<TabComponent>
+
+  ngAfterContentInit (): void {
+    // if (!this.tabs) return
+    // const activeTabs = this.tabs?.filter(tab => !!tab.active)
+    // const tabsNonActives = !activeTabs || activeTabs.length === 0
+    // if (tabsNonActives) {
+    //   this.selectTab(this.tabs.first)
+    // }
+  }
+
+  selectTab (tab: TabComponent, event?: Event): void {
+    event?.preventDefault()
+    this.tabs?.forEach((tab) => { tab.active = false })
+    tab.active = true
   }
 }
